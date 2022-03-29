9a173b495c37b594500af5aad0b826fe02e2b403
California Sullivan <california.l.sullivan@intel.com>
2022-03-28 21:40:35 -0400

    spr-kernel: fix build.sh shellcheck issues
    Signed-off-by: California Sullivan <california.l.sullivan@intel.com>

--- build/rhel-8/intel-mvp-spr-kernel/build.sh
+++ build/rhel-8/intel-mvp-spr-kernel/build.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-CURR_DIR=$(dirname "$(readlink -f "$0")")
+CURR_DIR="$(dirname "$(readlink -f "$0")")"
 
 DOWNSTREAM_GIT_URI="https://github.com/intel/linux-kernel-dcp.git"
 DOWNSTREAM_TAG="SPR-BKC-PC-v4.20"
 
 SPEC_FILE="${CURR_DIR}/spr-kernel.spec"
-RPMBUILD_DIR=${CURR_DIR}/rpmbuild
+RPMBUILD_DIR="${CURR_DIR}/rpmbuild"
 
 # Workaround to empty files not being downloadable
 EMPTY_SOURCES=" \
@@ -25,29 +25,29 @@ EMPTY_SOURCES=" \
 prepare() {
     echo "Prepare..."
 
-    if [[ ! -f ${RPMBUILD_DIR}/SOURCES/linux-spr-kernel.tar.gz ]]; then
+    if [[ ! -f "${RPMBUILD_DIR}/SOURCES/linux-spr-kernel.tar.gz" ]]; then
         if [[ ! -d linux-spr-kernel ]]; then
             git clone -b ${DOWNSTREAM_TAG} --single-branch ${DOWNSTREAM_GIT_URI} linux-spr-kernel
         fi
         tar --exclude=.git -czf linux-spr-kernel.tar.gz linux-spr-kernel
-        mv linux-spr-kernel.tar.gz ${RPMBUILD_DIR}/SOURCES/
+        mv linux-spr-kernel.tar.gz "${RPMBUILD_DIR}"/SOURCES/
     fi
 
-    cp ${CURR_DIR}/*.config ${RPMBUILD_DIR}/SOURCES/
-    cp ${CURR_DIR}/extra-sources/* ${RPMBUILD_DIR}/SOURCES/
+    cp "${CURR_DIR}"/*.config "${RPMBUILD_DIR}"/SOURCES/
+    cp "${CURR_DIR}"/extra-sources/* "${RPMBUILD_DIR}"/SOURCES/
     for f in $EMPTY_SOURCES; do
-        echo "" > ${RPMBUILD_DIR}/SOURCES/$f
+        echo "" > "${RPMBUILD_DIR}"/SOURCES/"$f"
     done
 }
 
 build() {
     echo "Build..."
-    sudo -E dnf builddep -y ${SPEC_FILE}
-    rpmbuild --define "_topdir ${RPMBUILD_DIR}" --undefine=_disable_source_fetch -v -ba ${SPEC_FILE}
+    sudo -E dnf builddep -y "${SPEC_FILE}"
+    rpmbuild --define "_topdir ${RPMBUILD_DIR}" --undefine=_disable_source_fetch -v -ba "${SPEC_FILE}"
 }
 
-pushd ${CURR_DIR}
-mkdir -p ${RPMBUILD_DIR}/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+pushd "${CURR_DIR}"
+mkdir -p "${RPMBUILD_DIR}"/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 prepare
 build
 popd
