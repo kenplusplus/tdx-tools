b55d4d7d3185ed08ad11c20adcb9df93c0a9075b
Feng, Jialei <jialei.feng@intel.com>
2022-06-02 22:53:25 -0400

    ubuntu 22.04: refine guest image tdx-guest-stack.sh
    Signed-off-by: Feng, Jialei <jialei.feng@intel.com>

--- build/ubuntu-22.04/guest-image/tdx-guest-stack.sh
+++ build/ubuntu-22.04/guest-image/tdx-guest-stack.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -x
+
 export LIBGUESTFS_BACKEND=direct
 
 THIS_DIR=$(dirname "$(readlink -f "$0")")
@@ -7,10 +9,11 @@ THIS_DIR=$(dirname "$(readlink -f "$0")")
 IMG_URL=https://cloud-images.ubuntu.com/jammy/current
 CLOUD_IMG=jammy-server-cloudimg-amd64.img
 TD_IMG=td-guest-ubuntu-22.04.qcow2
-REPO_DIR="${THIS_DIR}/../repo"
+REPO_NAME="repo"
+REPO_LOCAL=${THIS_DIR}/../${REPO_NAME}
 
-if ! readlink -f ${REPO_DIR} ; then
-    echo "${REPO_DIR} does not exist, please build it via build-repo.sh"
+if ! readlink -f ${REPO_LOCAL} ; then
+    echo "${REPO_LOCAL} does not exist, please build it via build-repo.sh"
     exit 1
 fi
 
@@ -29,18 +32,18 @@ cp $CLOUD_IMG $TD_IMG
 virt-customize -a ${TD_IMG} --root-password password:123456
 qemu-img resize ${TD_IMG} +5G
 
-ARGS=" -a ${TD_IMG} -v -x"
+ARGS=" -a ${TD_IMG} -x"
 
 # Setup guest environments
 ARGS+=" --copy-in /etc/environment:/etc"
 ARGS+=" --copy-in netplan.yaml:/etc/netplan/"
-ARGS+=" --copy-in ${REPO_DIR}:/srv/"
+ARGS+=" --copy-in ${REPO_LOCAL}:/srv/"
 ARGS+=" --edit '/etc/ssh/sshd_config:s/#PermitRootLogin prohibit-password/PermitRootLogin yes/'"
 ARGS+=" --edit '/etc/ssh/sshd_config:s/PasswordAuthentication no/PasswordAuthentication yes/'"
 ARGS+=" --run-command 'growpart /dev/sda 1'"
 ARGS+=" --run-command 'resize2fs /dev/sda1'"
 ARGS+=" --run-command 'ssh-keygen -A'"
-ARGS+=" --run-command 'dpkg -i /srv/tdx-guest-debs/linux-*.deb'"
+ARGS+=" --run-command 'dpkg -i /srv/${REPO_NAME}/linux-*.deb'"
 
 echo "${ARGS}"
 eval virt-customize "${ARGS}"
