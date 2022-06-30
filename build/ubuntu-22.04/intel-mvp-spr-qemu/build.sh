3af2a454f9f74db86da95c5d06177e599ab56354
jialeie <jialei.feng@intel.com>
2022-06-30 16:52:47 +0800

    ubuntu 22.04: initialize qemu package
    The debian package files are based on:
    http://archive.ubuntu.com/ubuntu/pool/main/q/qemu/qemu_6.2+dfsg-2ubuntu6.debian.tar.xz
    And modified to only build for qemu-system-x86.
    QEMU source: https://github.com/intel/qemu-dcp
    
    Signed-off-by: jialeie <jialei.feng@intel.com>

---
+++ build/ubuntu-22.04/intel-mvp-spr-qemu/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# set -ex
+
+CURR_DIR=$(dirname "$(readlink -f "$0")")
+UPSTREAM_VERSION="6.2.0"
+DOWNSTREAM_GIT_URI="https://github.com/intel/qemu-dcp.git"
+
+DOWNSTREAM_TAG="SPR-BKC-QEMU-v2.5"
+PACKAGE="mvp-spr-qemu"
+
+if [[ $(grep "Ubuntu" /etc/os-release) == "" ]]; then
+    echo "Please build the packages in Ubuntu"
+    exit 1
+fi
+
+get_source() {
+    echo "Get downstream source code..."
+    cd ${CURR_DIR}
+    if [[ ! -d ${PACKAGE}-${UPSTREAM_VERSION} ]]; then
+	# add safe directory for running in docker
+	git config --global --add safe.directory /github/workspace
+	git config --global --add safe.directory /repo
+        git clone --branch ${DOWNSTREAM_TAG} --single-branch --depth 1 \
+            ${DOWNSTREAM_GIT_URI} ${PACKAGE}-${UPSTREAM_VERSION}
+        cd ${PACKAGE}-${UPSTREAM_VERSION}
+        git submodule update --init
+    fi
+}
+
+prepare() {
+    echo "Prepare..."
+    cp ${CURR_DIR}/debian/ ${CURR_DIR}/${PACKAGE}-${UPSTREAM_VERSION} -fr
+
+    sudo apt update
+    sudo DEBIAN_FRONTEND=noninteractive TZ=Asia/Shanghai apt install tzdata -y
+}
+
+build() {
+    echo "Build..."
+    cd ${CURR_DIR}/${PACKAGE}-${UPSTREAM_VERSION}
+    sudo -E mk-build-deps --install --build-dep --build-indep '--tool=apt-get --no-install-recommends -y' debian/control
+    dpkg-source --before-build .
+    debuild -uc -us -b
+}
+
+get_source
+prepare
+build
