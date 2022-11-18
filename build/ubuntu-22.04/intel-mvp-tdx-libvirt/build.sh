ae61ac8a3b6a50ae0caf93ef997c629004f2bbfb
Feng, Jialei <jialei.feng@intel.com>
2022-11-18 04:10:38 -0500

    build: add libvirt support for ubuntu 22.04 tdx 5.19 stack
    Signed-off-by: Feng, Jialei <jialei.feng@intel.com>

--- build/ubuntu-22.04/intel-mvp-tdx-libvirt/build.sh
+++ build/ubuntu-22.04/intel-mvp-tdx-libvirt/build.sh
@@ -4,14 +4,14 @@ set -e
 
 CURR_DIR=$(dirname "$(readlink -f "$0")")
 
-UPSTREAM_URI="https://libvirt.org/sources/libvirt-8.1.0.tar.xz"
-UPSTREAM_VERSION="8.1.0"
-UPSTREAM_BASE_COMMIT="5dd76de22578d331585c1fe03f486c43acbd3588"
+UPSTREAM_URI="https://libvirt.org/sources/libvirt-8.6.0.tar.xz"
+UPSTREAM_VERSION="8.6.0"
+UPSTREAM_BASE_COMMIT="f8b6c7e59afd1b700d446239922ee097a6f1433c"
 DOWNSTREAM_GIT_URI="https://github.com/intel/libvirt-tdx.git"
-DOWNSTREAM_TAG="tdx-libvirt-2022.04.20"
+DOWNSTREAM_TAG="tdx-libvirt-2022.11.17"
 
 PACKAGE="tdx-libvirt"
-PACKAGE_VERSION="2022.04.20"
+PACKAGE_VERSION="2022.11.17"
 
 UPTRREAM_FILE="${UPSTREAM_URI##*/}"
 DOWNSTREAM_VERSION=${DOWNSTREAM_TAG#tdx-libvirt-}
@@ -42,7 +42,7 @@ prepare() {
     echo "**** Prepare ****"
     cp "${CURR_DIR}"/debian/ "${CURR_DIR}"/${PACKAGE}-${PACKAGE_VERSION} -fr
     cd "${CURR_DIR}"/${PACKAGE}-${PACKAGE_VERSION}
-    tar -xf libvirt-8.1.0.tar.xz --strip-components=1 --directory "${CURR_DIR}"/${PACKAGE}-${PACKAGE_VERSION}
+    tar -xf libvirt-${UPSTREAM_VERSION}.tar.xz --strip-components=1 --directory "${CURR_DIR}"/${PACKAGE}-${PACKAGE_VERSION}
 }
 
 build() {
