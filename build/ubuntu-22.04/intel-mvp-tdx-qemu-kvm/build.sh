9940a1659f3ebec454eb2bf6d49c7b64e422b0ee
jialeie <jialei.feng@intel.com>
2023-04-06 16:04:57 +0800

    github/workflows: fix ShellCheck and rhel-8 qemu trigger path
    Signed-off-by: jialeie <jialei.feng@intel.com>

--- build/ubuntu-22.04/intel-mvp-tdx-qemu-kvm/build.sh
+++ build/ubuntu-22.04/intel-mvp-tdx-qemu-kvm/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 CURR_DIR=$(dirname "$(readlink -f "$0")")
 UPSTREAM_VERSION="7.0.50"
 UPSTREAM_GIT_URI="https://github.com/qemu/qemu.git"
 UPSTREAM_TAG="ad4c7f529a279685da84297773b4ec8080153c2d"
-PACKAGE="mvp-tdx-qemu"
+PACKAGE="mvp-tdx-qemu-${UPSTREAM_VERSION}"
 PATCHSET="${CURR_DIR}/../../common/patches-tdx-qemu-MVP-QEMU-7.0-v1.3.tar.gz"
 
 if [[ $(grep "Ubuntu" /etc/os-release) == "" ]]; then
@@ -14,11 +16,11 @@ fi
 
 get_source() {
     echo "Get downstream source code..."
-    cd ${CURR_DIR}
-    if [[ ! -d ${PACKAGE}-${UPSTREAM_VERSION} ]]; then
-        git clone ${UPSTREAM_GIT_URI} ${PACKAGE}-${UPSTREAM_VERSION}
+    cd "${CURR_DIR}"
+    if [[ ! -d ${PACKAGE} ]]; then
+        git clone ${UPSTREAM_GIT_URI} ${PACKAGE}
         tar xf "${PATCHSET}"
-        cd ${PACKAGE}-${UPSTREAM_VERSION}
+        cd ${PACKAGE}
         git checkout ${UPSTREAM_TAG}
         git config user.name "${USER:-tdx-builder}"
         git config user.email "${USER:-tdx-builder}"@"$HOSTNAME"
@@ -31,7 +33,7 @@ get_source() {
 
 prepare() {
     echo "Prepare..."
-    cp ${CURR_DIR}/debian/ ${CURR_DIR}/${PACKAGE}-${UPSTREAM_VERSION} -fr
+    cp "${CURR_DIR}"/debian/ "${CURR_DIR}"/"${PACKAGE}" -fr
 
     sudo apt update
     sudo apt install systemd -y
@@ -40,7 +42,7 @@ prepare() {
 
 build() {
     echo "Build..."
-    cd ${CURR_DIR}/${PACKAGE}-${UPSTREAM_VERSION}
+    cd "${CURR_DIR}"/${PACKAGE}
     sudo -E mk-build-deps --install --build-dep --build-indep '--tool=apt-get --no-install-recommends -y' debian/control
     dpkg-source --before-build .
     debuild -uc -us -b
