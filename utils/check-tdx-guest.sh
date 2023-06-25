59f2a64714e50387153c87a69ba7dc33ca6c93b2
intelzhongjie <zhongjie.shi@intel.com>
2023-06-25 09:33:35 +0800

    check-tdx-guest.sh: add more check (#361)
    This change will resolve issue:
    https://github.com/intel/tdx-tools/issues/342
    
    Signed-off-by: zhongjie <zhongjie.shi@intel.com>

--- utils/check-tdx-guest.sh
+++ utils/check-tdx-guest.sh
@@ -4,8 +4,27 @@
 #
 
 tdx_cpuinfo=$(grep -o tdx_guest /proc/cpuinfo)
-if [[ $tdx_cpuinfo == "tdx_guest" ]]; then
-  echo TDX guest is enabled!
-else
-  echo TDX guest is not enabled!
+if [[ $tdx_cpuinfo != "tdx_guest" ]]; then
+  echo "This is NOT a TDX guest!"
+  echo "No config in cpuinfo!"
+  exit 1
 fi
+
+if [[ ! -e /sys/firmware/acpi/tables/CCEL ]]; then
+  echo "This is NOT a TDX guest!"
+  echo "No CCEL table!"
+  echo "Please reference:"
+  echo "    https://www.intel.com/content/www/us/en/developer/articles/technical/intel-trust-domain-extensions.html"
+  echo "    https://uefi.org/specs/ACPI/6.5/05_ACPI_Software_Programming_Model.html#cc-event-log-acpi-table"
+  exit 1
+fi
+
+if [[ ! -e /dev/tdx_guest ]]; then
+  echo "This is NOT a TDX guest!"
+  echo "No tdx_guest device!"
+  echo "Please reference:"
+  echo "    https://docs.kernel.org/virt/coco/tdx-guest.html"
+  exit 1
+fi
+
+echo This is a TDX guest!
