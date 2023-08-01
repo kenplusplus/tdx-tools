f645b219444a3a9a01c09b5626a99867005e6260
BFuhry <89401140+BFuhry@users.noreply.github.com>
2023-08-01 16:55:55 +0200

    kernel: allow pahole installation to downgrade version (#435)
    Allow the automatic downgrade of pahole during the installation, to also
    fix the "Load BTF from vmlinux: Invalid argument" issue on systems with
    an already newer pahole version.
    
    Signed-off-by: Benny Fuhry <benny.fuhry@intel.com>

--- build/ubuntu-22.04/intel-mvp-tdx-kernel/build.sh
+++ build/ubuntu-22.04/intel-mvp-tdx-kernel/build.sh
@@ -38,7 +38,7 @@ prepare() {
     cp "${CURR_DIR}"/linux-6.2.16/* "${SOURCE_DIR}" -fr
 
     sudo apt update
-    sudo apt install pahole=1.22-8 -y
+    sudo apt install pahole=1.22-8 -y --allow-downgrades
     if [[ -f /etc/timezone ]]; then
         sudo DEBIAN_FRONTEND=noninteractive apt install tzdata -y
     else
