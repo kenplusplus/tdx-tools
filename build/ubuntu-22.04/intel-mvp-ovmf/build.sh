cb319df1d945ad38836fd5337b945abc6a8b4eb3
Jialei Feng <jialei.feng@intel.com>
2023-09-20 14:20:57 +0800

    ubuntu-22.04: update edk2-staging 2023-tdvf-ww36.2 (#492)
    Signed-off-by: Feng, Jialei <jialei.feng@intel.com>

--- build/ubuntu-22.04/intel-mvp-ovmf/build.sh
+++ build/ubuntu-22.04/intel-mvp-ovmf/build.sh
@@ -3,11 +3,10 @@
 set -e
 
 CURR_DIR=$(dirname "$(readlink -f "$0")")
-UPSTREAM_GIT_URI="https://github.com/tianocore/edk2.git"
-UPSTREAM_TAG="edk2-stable202302"
+UPSTREAM_GIT_URI="https://github.com/tianocore/edk2-staging.git"
+UPSTREAM_TAG="2023-tdvf-ww36.2"
 
-REPO_FOLDER="ovmf-stable202302"
-PATCHSET="${CURR_DIR}/../../common/patches-ovmf-edk2-stable202302-ww27.2.tar.gz"
+REPO_FOLDER="ovmf-ww36.2"
 
 get_origin() {
     echo "**** Download origin package ****"
@@ -15,12 +14,6 @@ get_origin() {
     if [[ ! -f ${CURR_DIR}/edk2.tar.gz ]]; then
         git clone --branch ${UPSTREAM_TAG} ${UPSTREAM_GIT_URI} ${REPO_FOLDER}
         pushd ${REPO_FOLDER}
-        tar xf "${PATCHSET}"
-        git config user.name "${USER:-tdx-builder}"
-        git config user.email "${USER:-tdx-builder}"@"$HOSTNAME"
-        for i in patches/*; do
-           git am --keep-cr "$i"
-        done
         git submodule init
         git submodule sync
         git submodule update
